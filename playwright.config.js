86152318b6977292fd257fed2d09ed91d3883025
rafaelp <raffypmir@gmail.com>
2025-11-10 18:33:20 -0600

    changed worker

--- playwright.config.js
+++ playwright.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: 1,
   /* Opt out of parallel tests on CI. */
-  workers: 4,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   
